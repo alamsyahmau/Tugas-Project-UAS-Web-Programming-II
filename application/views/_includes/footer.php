ba426962de2762311d41d50d14d35bba0cc91e52
alamsyahmau <12210901@bsi.ac.id>
2022-12-04 15:21:44 +0700

    menambahkan folder project Web Programming II by: Alamsyah Maulana

---
+++ application/views/_includes/footer.php
@@ -0,0 +1,12 @@
+<div class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                 Copyright © 2022 Sop Durian Abnurmal. All rights reserved.</a>.
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
